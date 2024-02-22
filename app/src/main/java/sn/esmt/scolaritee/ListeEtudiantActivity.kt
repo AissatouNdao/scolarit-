ba5f3cb4c1a7798fbc6441770c8f40e0397e24f9
unknown <aissatou.ndao.etu@esmt.sn>
2024-02-22 10:13:34 +0100

    livraison du projet android

---
+++ app/src/main/java/sn/esmt/scolaritee/ListeEtudiantActivity.kt
@@ -0,0 +1,13 @@
+package sn.esmt.scolaritee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListeEtudiantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_liste_etudiant)
+
+
+    }
+}
